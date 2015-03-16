ef3487b3d691156c7a325d11a5d3f978af4d41d3
Chris Kottom <chris@chriskottom.com>
2015-03-16 15:12:52 +0100

    adding initial assertion logic, tests

--- lib/minitest/flog.rb
+++ lib/minitest/flog.rb
@@ -1,7 +1,65 @@
+require "flog_cli"
 require "minitest/flog/version"
+require "minitest/test"
 
 module Minitest
   module Flog
-    # Your code goes here...
+    def self.flogs
+      @flogs ||= {}
+    end
+
+    module Assertions
+      DEFAULT_FLOG_OPTIONS   = { all: true, continue: true }
+      DEFAULT_METHOD_OPTIONS = { score: 40.0, ratio: 1.0 }
+
+      def assert_method_score(dir, options = {})
+        options = DEFAULT_METHOD_OPTIONS.merge options
+        scores = _flog_scores_for_directory(dir)
+        filtered = scores.select { |method, score| score > options[:score] }
+
+        score_count = scores.count.to_f.round(2)
+        filtered_count = filtered.count.to_f.round(2)
+        msg = options[:msg] || message(options[:score], options[:ratio],
+                                       1.0 - (filtered_count / score_count))
+        assert (filtered_count / score_count) <= (1.0 - options[:ratio]), msg
+      end
+
+      def _flog_scores_for_directory(dir)
+        flog = flog_for(dir)
+        scores = {}
+        flog.each_by_score do |method, score, list|
+          scores[method] = score
+        end
+        scores
+      end
+
+      private
+
+      def flog_for(dir)
+        hash = FlogCLI.expand_dirs_to_files(dir).hash
+
+        unless flog = Minitest::Flog.flogs[hash]
+          flog = FlogCLI.new DEFAULT_FLOG_OPTIONS
+          result = flog.flog dir
+          Minitest::Flog.flogs[hash] = flog
+        end
+        flog
+      end
+
+      def message(score, expected_ratio, actual_ratio)
+        expected_percent = sprintf("%.1f", expected_ratio * 100)
+        actual_percent = sprintf("%.1f", actual_ratio * 100)
+        "Expected #{ expected_percent }% of flogged methods to have " +
+          "score < #{ score } (actual: #{ actual_percent }%)"
+      end
+    end
+
+    class Test < ::Minitest::Test
+      include Assertions
+
+      def self.runnable_methods
+        methods_matching(/^flog_/)
+      end
+    end
   end
 end
--- test/flog_test.rb
+++ test/flog_test.rb
@@ -1,4 +1,89 @@
 require 'test_helper'
 
 class FlogTest < Minitest::Test
+  def test_runnable_methods_correct
+    test_flogger = Class.new Minitest::Flog::Test do
+      def flog_this; end
+      def flog_that; end
+      def dont_flog_this; end
+      private
+      def flog_something_else; end
+    end
+
+    runnable_methods = test_flogger.runnable_methods.sort
+    assert_equal %w(flog_that flog_this), runnable_methods
+  end
+end
+
+class FlogAssertionsTest < Minitest::Test
+  # With just a directory, the assert defaults to max score = 40, ratio = 1.0.
+  def test_assert_method_score_just_directory
+    flogger_class = Class.new Minitest::Flog::Test do
+      def flog_this
+        assert_method_score("some_dir")
+      end
+    end
+
+    flogger = flogger_class.new(:flog_this)
+    flogger.stub(:_flog_scores_for_directory, { "foo" => 40.0 }) do
+      result = flogger.run
+      assert result.passed?, "Expected the test to pass, but it didn't: #{ result.result_code }"
+    end
+
+    flogger.stub(:_flog_scores_for_directory, { "foo" => 40.1 }) do
+      result = flogger.run
+      failure = result.failure
+      assert_instance_of Minitest::Assertion, failure
+
+      msg = "Expected 100.0% of flogged methods to have score < 40.0 (actual: 0.0%)"
+      assert_equal msg, failure.message
+    end
+  end
+
+  def test_assert_method_score_with_score
+    flogger_class = Class.new Minitest::Flog::Test do
+      def flog_that
+        assert_method_score("some_dir", score: 50.0)
+      end
+    end
+
+    flogger = flogger_class.new(:flog_that)
+    flogger.stub(:_flog_scores_for_directory, { "foo" => 50.0 }) do
+      result = flogger.run
+      assert result.passed?, "Expected the test to pass, but it didn't: #{ result.result_code }"
+    end
+
+    flogger.stub(:_flog_scores_for_directory, { "foo" => 50.1 }) do
+      result = flogger.run
+      failure = result.failure
+      assert_instance_of Minitest::Assertion, failure
+
+      msg = "Expected 100.0% of flogged methods to have score < 50.0 (actual: 0.0%)"
+      assert_equal msg, failure.message
+    end
+  end
+
+  def test_assert_method_score_with_score_and_ratio
+    flogger_class = Class.new Minitest::Flog::Test do
+      def flog_everything
+        assert_method_score("some_dir", score: 50.0, ratio: 0.5)
+      end
+    end
+
+    flogger = flogger_class.new(:flog_everything)
+    flogger.stub(:_flog_scores_for_directory, { "foo" => 49.0, "bar" => 51.0 }) do
+      result = flogger.run
+      assert result.passed?, "Expected the test to pass, but it didn't: #{ result.result_code }"
+    end
+
+    flogger.stub(:_flog_scores_for_directory,
+                 { "foo" => 50.1, "bar" => 51.0, "bat" => 49.0 }) do
+      result = flogger.run
+      failure = result.failure
+      assert_instance_of Minitest::Assertion, failure
+
+      msg = "Expected 50.0% of flogged methods to have score < 50.0 (actual: 33.3%)"
+      assert_equal msg, failure.message
+    end
+  end
 end
